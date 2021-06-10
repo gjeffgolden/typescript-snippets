c399a857d2035e6dfdcbab7ff70d3021827f18d5
gjeffgolden <gjgolden08@gmail.com>
2021-06-10 12:45:49 -0600

    Adding inheritance logic to classes.ts

--- basic-types/classes.ts
+++ basic-types/classes.ts
@@ -1,25 +1,39 @@
-class Vehicle {
-    public drive(): void {
-        console.log('VROOM VROOM')
-    }
+//public => called anywhere, anytime (default)
+//private => can be called by other methods within THIS class
+//protected => can be called by other methods in THIS class, or by other methods within CHILD classes
 
-    public honk(): void {
+class Vehicle {
+    
+    constructor(public color: string) {}
+
+    protected honk(): void {
         console.log('BEEP BEEP')
     }
 }
 
+const vehicle = new Vehicle('orange')
+console.log(vehicle.color)
+
 //inheritance
 class Car extends Vehicle {
+    constructor(public wheels: number, color: string) {
+        super(color)
+        //super is a reference to the constructor in parent class
+    }
+
     private drive(): void {
         console.log('SKRRRRRRT')
     }
 
     startDrivingProcess(): void {
         this.drive()
+        this.honk()
     }
 }
 
-const car = new Car()
+const car = new Car(4, 'red')
 
 car.startDrivingProcess()
-car.honk()
+
+//doesn't work because honk() is marked as protected or private
+//car.honk()
